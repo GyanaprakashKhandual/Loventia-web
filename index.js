382d049742fb421c82e7b5ec544379416f83be37
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-17 17:45:38 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
 
+const connectDB = require('./config/DB');
+
+
+dotenv.config();
 const app = express();
 app.use(express.json());
 
+connectDB();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,7 +20,7 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
